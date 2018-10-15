30f5add6784a2b41948e73c3be35508afad41f2a
Amanpreet Singh <coool.aman@gmail.com>
2018-10-15 12:38:07 +0530

    Added Experiment

---
+++ core/src/main/java/com/fun/Experiment.java
@@ -0,0 +1,10 @@
+package com.fun;
+/**
+ * @author amanpreet.singh
+ * @version 1.0.0
+ * @since 2018-10-15 12:15.
+ */
+public class Experiment {
+    public static void main(String[] args) {
+    }
+}
